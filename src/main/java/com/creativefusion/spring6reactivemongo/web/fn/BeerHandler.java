6e4fd819108a5d6cfa0f3926aa46f4aff0e1ca24
sadat.malik <sadat.malik@me.com>
2023-04-27 11:02:47 +0100

    adding a webflux.fn handler

---
+++ src/main/java/com/creativefusion/spring6reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,23 @@
+package com.creativefusion.spring6reactivemongo.web.fn;
+
+import com.creativefusion.spring6reactivemongo.model.BeerDTO;
+import com.creativefusion.spring6reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author sm@creativefusion.net
+ */
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request){
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
